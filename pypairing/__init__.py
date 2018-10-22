77410bf07ff7f1c56d173ff425a4562a79fdbe67
Rahul Mahadev <rahul.mahadev7@gmail.com>
2018-10-22 15:30:58 -0500

    added pyfr (#3)
    * Added mul_assign to G1 and G2
    
    * Added PyFrRepr

--- pypairing/__init__.py
+++ pypairing/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from .pypairing import PyG1, PyG2, py_pairing, PyFq12
+from .pypairing import PyG1, PyG2, py_pairing, PyFq12, PyFr, PyFrRepr
 
-__all__ = ["PyG1", "PyFr", "PyG2", "py_pairing", "PyFq12"]
+__all__ = ["PyG1", "PyFr", "PyFrRepr", "PyG2", "py_pairing", "PyFq12"]
 
 
 
